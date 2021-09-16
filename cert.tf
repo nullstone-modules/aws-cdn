211481504955dc837fed1b51f16d9d3bcd5a2926
Brad Sickles <brad.sickles@gmail.com>
2021-09-16 08:27:03 -0400

    Converting module to be a capability (#4)

--- cert.tf
+++ cert.tf
@@ -1,20 +1,12 @@
 module "cert" {
-  source = "nullstone-modules/sslcert/aws"
+  source  = "nullstone-modules/sslcert/aws"
+  enabled = true
 
   domain = {
-    name    = local.main_subdomain
-    zone_id = local.main_zone_id
+    name    = local.subdomain
+    zone_id = local.zone_id
   }
 
   alternative_names = local.alt_subdomains
-  tags              = data.ns_workspace.this.tags
-  enabled           = local.has_domain
-
-  providers = {
-    aws.domain = aws.domain
-  }
-}
-
-locals {
-  cert_arn = local.has_domain ? module.cert[0].certificate_arn : data.ns_connection.subdomain.outputs.cert_arn
+  tags              = local.tags
 }
--- nullstone.tf
+++ nullstone.tf
@@ -8,34 +8,16 @@ terraform {
 
 data "ns_workspace" "this" {}
 
-data "ns_connection" "domain" {
-  name     = "domain"
-  type     = "domain/aws"
-  optional = true
-}
-
 data "ns_connection" "subdomain" {
-  name     = "subdomain"
-  type     = "subdomain/aws"
-  optional = true
-}
-
-data "ns_connection" "origin" {
-  name = "origin"
-  type = "site/aws-s3"
+  name = "subdomain"
+  type = "subdomain/aws"
 }
 
 locals {
-  has_domain    = data.ns_connection.domain.workspace_id != ""
-  has_subdomain = data.ns_connection.subdomain.workspace_id != ""
-  zone_id       = try(data.ns_connection.domain.outputs.zone_id, data.ns_connection.subdomain.outputs.zone_id)
-}
+  tags = data.ns_workspace.this.tags
 
-// We will need to be able to support secondary providers since the root domain
-//   is typically managed in a separate account from non-production environments
-provider "aws" {
-  access_key = try(data.ns_connection.domain.outputs.delegator["access_key"], "")
-  secret_key = try(data.ns_connection.domain.outputs.delegator["secret_key"], "")
-
-  alias = "domain"
+  zone_id        = data.ns_connection.subdomain.outputs.zone_id
+  subdomain      = data.ns_connection.subdomain.outputs.fqdn
+  alt_subdomains = var.enable_www ? ["www.${local.subdomain}"] : []
+  all_subdomains = concat([local.subdomain], local.alt_subdomains)
 }
