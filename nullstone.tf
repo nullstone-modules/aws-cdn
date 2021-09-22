c6d8d36136ddc99c5ec044cb3b7a1692628d036a
Brad Sickles <brad.sickles@gmail.com>
2021-09-22 19:11:46 -0400

    Fixed domain provider

--- nullstone.tf
+++ nullstone.tf
@@ -20,13 +20,11 @@ locals {
   subdomain      = data.ns_connection.subdomain.outputs.fqdn
   alt_subdomains = var.enable_www ? ["www.${local.subdomain}"] : []
   all_subdomains = concat([local.subdomain], local.alt_subdomains)
-
-  domain_delegator = try(data.ns_connection.subdomain.outputs.delegator, { access_key : "", secret_key : "" })
 }
 
 provider "aws" {
-  access_key = local.domain_delegator.access_key
-  secret_key = local.domain_delegator.secret_key
+  access_key = try(data.ns_connection.subdomain.outputs.delegator["access_key"], "")
+  secret_key = try(data.ns_connection.subdomain.outputs.delegator["secret_key"], "")
 
   alias = "domain"
 }
