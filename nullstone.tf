c4b3a8a48b6724cfef4cf8d1abd332511d690826
Brad Sickles <brad.sickles@gmail.com>
2022-06-07 10:50:27 -0400

    Standardize

--- nullstone.tf
+++ nullstone.tf
@@ -8,31 +8,17 @@ terraform {
 
 data "ns_workspace" "this" {}
 
-data "ns_connection" "subdomain" {
-  name = "subdomain"
-  type = "subdomain/aws"
+// Generate a random suffix to ensure uniqueness of resources
+resource "random_string" "resource_suffix" {
+  length  = 5
+  lower   = true
+  upper   = false
+  numeric = false
+  special = false
 }
 
 locals {
-  tags = data.ns_workspace.this.tags
-
-  zone_id        = data.ns_connection.subdomain.outputs.zone_id
-  subdomain      = trimsuffix(data.ns_connection.subdomain.outputs.fqdn, ".")
-  alt_subdomains = var.enable_www ? ["www.${local.subdomain}"] : []
-  all_subdomains = concat([local.subdomain], local.alt_subdomains)
-}
-
-provider "aws" {
-  access_key = try(data.ns_connection.subdomain.outputs.delegator["access_key"], "")
-  secret_key = try(data.ns_connection.subdomain.outputs.delegator["secret_key"], "")
-
-  alias = "domain"
-}
-
-// When attaching certificates to a CDN, the cert must live in the us-east-1 region
-// See https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-invalid-viewer-certificate/
-provider "aws" {
-  region = "us-east-1"
-
-  alias = "cert"
+  tags          = data.ns_workspace.this.tags
+  block_name    = data.ns_workspace.this.block_name
+  resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 }
---
+++ subdomain.tf
@@ -0,0 +1,27 @@
+data "ns_connection" "subdomain" {
+  name = "subdomain"
+  type = "subdomain/aws"
+}
+
+locals {
+  zone_id        = data.ns_connection.subdomain.outputs.zone_id
+  subdomain      = trimsuffix(data.ns_connection.subdomain.outputs.fqdn, ".")
+  alt_subdomains = var.enable_www ? ["www.${local.subdomain}"] : []
+  all_subdomains = concat([local.subdomain], local.alt_subdomains)
+  delegator      = data.ns_connection.subdomain.outputs.delegator
+}
+
+provider "aws" {
+  access_key = try(local.delegator["access_key"], "")
+  secret_key = try(local.delegator["secret_key"], "")
+
+  alias = "domain"
+}
+
+// When attaching certificates to a CDN, the cert must live in the us-east-1 region
+// See https://aws.amazon.com/premiumsupport/knowledge-center/cloudfront-invalid-viewer-certificate/
+provider "aws" {
+  region = "us-east-1"
+
+  alias = "cert"
+}
